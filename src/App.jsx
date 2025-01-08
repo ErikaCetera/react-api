223772e9082ca880dd3acb125d94ecd3b235dcf1
ErikaCetera <ceteraerika@gmail.com>
2025-01-08 15:14:13 +0100

    feat: componenti Card e Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { AppCard } from './components/AppCard';
+import { AppForm } from './components/AppForm';
 
 
 const initialFormData = {
@@ -8,14 +10,15 @@ const initialFormData = {
   immagine: ""
 };
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 function App() {
-  // Definisce uno stato chiamato 'title' 
+
+  //Variabili di stato 
   const [articles, setArticles] = useState([]);
   const [formData, setFormData] = useState(initialFormData);
 
-
-
-// Funzione per gestire il cambiamento del valore dell'input
+  // Funzione per gestire il cambiamento del valore dell'input
   const handleInputChange = (event) => {
     const keyToChange = event.target.name;
 
@@ -26,56 +29,45 @@ function App() {
 
     setFormData(newData);
   };
-   const url = "http://localhost:3003"
 
- useEffect(() => {
-  getPosts()
- }, []);
+  //Mostra i posts all'avvio della pagina
+  useEffect(() => {
+    getPosts()
+  }, []);
 
- const getPosts = () => {
-  axios.get(`${url}/posts`).then((resp) =>{
-    
-    // console.log(resp.data.postsList);
-    
-    setArticles(resp.data.postsList)
-  });
- };
- console.log(articles);
- 
+  const getPosts = () => {
+    //Preleva lista dei posts dal server
+    axios.get(`${apiUrl}/posts`).then((resp) => {
+      setArticles(resp.data.postsList)
+    });
+  };
+  console.log(articles);
+
 
   // Funzione per gestire l'invio del form
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    axios.post(`${url}/posts`, formData).then((resp) => {
-      
-    
-
-    //creo oggetto nuovo articolo
-    const newArticle = {
-      ...formData,
-    }
-
-    //creo copia degli articoli precedenti
-    const newArray = [...articles, newArticle];
-
-    //aggiorno stato menu
-    setArticles(newArray)
-
-    //ripulisco stati del form
-    setFormData(initialFormData);
-  })
-
+    //Invia dati del form al server
+    axios.post(`${apiUrl}/posts`, formData).then((resp) => {
+      //creo oggetto nuovo articolo
+      const newArticle = {
+        ...formData,
+      }
+      //creo copia degli articoli precedenti
+      const newArray = [...articles, newArticle];
+      //aggiorno stato menu
+      setArticles(newArray)
+      //ripulisco stati del form
+      setFormData(initialFormData);
+    })
   };
 
-  const cancella = (idDaCancellare) => {
-    const newArray = articles.filter((curArt) => curArt.id !== idDaCancellare);
-    setArticles(newArray);
-    axios.delete(`${url}/posts/${idDaCancellare}`).then((resp) => {
+  // Funzione per gestire eliminazione dei posts 
+  const handleDelete = (idToDelete) => {
+    axios.delete(`${apiUrl}/posts/${idToDelete}`).then((resp) => {
+      const newArray = articles.filter((curItem) => curItem.id !== idToDelete);
+      setArticles(newArray);
     });
-  console.log(articles);
-  
-    
   };
 
   return (
@@ -86,21 +78,12 @@ function App() {
           <h2>Articoli</h2>
           {articles.length > 0 ? (
             <div className="row row-cols-2 row-cols-lg-3">
-              {articles.map((curItem) => (
-                <div className="col" key={curItem.id}>
-                  <div className="card">
-                    <div className="card-body">
-                      <h4>{curItem.titolo}</h4>
-                      <p>{curItem.contenuto}</p>
-                      <img src= {`${url} ${curItem.immagine}`}/>
-                      <button
-                        onClick={() => cancella(curItem.id)}
-                        className="btn btn-warning"
-                      >
-                        <i className="fas fa-trash-alt"></i>
-                      </button>
-                    </div>
-                  </div>
+              {articles.map((curItem, index) => (
+                <div className="col" key={index}>
+                  <AppCard
+                    post={curItem}
+                    onDelete={() => handleDelete(curItem.id)}
+                  />
                 </div>
               ))}
             </div>
@@ -109,57 +92,14 @@ function App() {
           )}
         </section>
 
-
-        <section>
+         <section>
           <div className='row w-50'>
             <h3 className='mb-3'>Aggiungi un Articolo</h3>
-            <form onSubmit={handleSubmit} >
-
-              {/* //TITOLO*/}
-              <div className='mb-3'>
-                <label className='form-label' htmlFor="article-title">Titolo</label>
-                <input
-                  className='form-control w-50'
-                  type="text" id='article-title'
-                  name='titolo'
-                  // Imposta il valore dell'input
-                  value={formData.titolo || ""}
-                  // Chiama handleChange quando il valore dell'input cambia
-                  onChange={handleInputChange}
-                />
-              </div>
-
-              {/* //CONTENUTO*/}
-              <div className='mb-3'>
-                <label className='form-label' htmlFor="article-content">Contenuto</label>
-                <textarea
-                  className='form-control '
-                  type="text" id='article-content'
-                  name='contenuto'
-                  // Imposta il valore dell'input
-                  value={formData.contenuto || ""}
-                  // Chiama handleChange quando il valore dell'input cambia
-                  onChange={handleInputChange}
-                />
-              </div>
-
-              {/* //IMMAGINE*/}
-              <div className='mb-3'>
-                <label className='form-label' htmlFor="article-image">Immagine</label>
-                <input
-                  className='form-control w-50'
-                  type="text" id='article-image'
-                  name='immagine'
-                  // Imposta il valore dell'input
-                  value={formData.immagine || ""}
-                  // Chiama handleChange quando il valore dell'input cambia
-                  onChange={handleInputChange}
-                />
-              </div>
-
-              <button type="submit" className="btn btn-success">Aggiungi</button>
-            </form>
-
+            <AppForm
+              form={formData}
+              handleInputChange={handleInputChange}
+              handleSubmit={handleSubmit}
+            />
           </div>
         </section>
       </main>
---
+++ src/components/AppCard.jsx
@@ -0,0 +1,24 @@
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export const AppCard = ({ post, onDelete }) => {
+
+    return (
+        <>
+            <div className="card">
+                <div className="card-body">
+                    <h4>{post.titolo}</h4>
+                    <p>{post.contenuto}</p>
+                    <img src={`${apiUrl}/${post.immagine}`} />
+                </div>
+            </div>
+            <div>
+                <button
+                    onClick={onDelete}
+                    className="btn btn-warning"
+                >
+                    <i className="fas fa-trash-alt"></i>
+                </button>
+            </div>
+        </>
+    )
+}
